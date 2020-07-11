ddbfffffaab3f5bc56cc2fb639188c86b188049d
hxx <>
2020-07-11 22:57:18 +0800

    第一次提交

---
+++ src/alert.tsx
@@ -0,0 +1,40 @@
+import React, {ReactNode} from "react";
+import ReactDOM from 'react-dom';
+import Modal from "antd-mobile/lib/modal/index";
+import { Action } from 'antd-mobile/lib/modal/PropsType'
+export default function alert(
+    title: ReactNode,
+    message: ReactNode,
+    actions = [{text:'知道了'}]
+) {
+    const stopMove = (e:any)=>e.preventDefault()
+   const div = document.createElement('div');
+   document.body.appendChild(div);
+   const close = ()=>{
+       ReactDOM.unmountComponentAtNode(div);
+       div.remove();
+       document.body.removeEventListener('touchmove',stopMove)
+   }
+   const footer = actions.map((button: Action<React.CSSProperties>) => {
+       const oldPress = button.onPress || (() => {})
+       button.onPress = () =>{
+           const res = oldPress();
+           if (res && res.then()){
+               res.then(close).catch(()=>{})
+           }else close()
+       }
+       return button;
+   })
+    document.body.addEventListener('touchmove',stopMove,{passive:false})
+    ReactDOM.render(
+        <Modal
+            visible={true}
+            transparent={true}
+            title={title}
+            closable={false}
+            maskClosable={false}
+            footer={footer}
+        ><div>{message}</div></Modal>,
+        div
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd-mobile/dist/antd-mobile.css'
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Alert from './alert';
+// import Operation from './operation';
+import Prompt from './prompt';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Alert('title','hell alert');
+// Operation([{text: '标为已读'},{text: '置顶聊天'}]);
+Prompt('title','hell alert');
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
